4bb6628659dee8259df0cb632a1d39c136fc7110
Timur Buchberger <timur,buch@users.noreplay.github.com>
2022-02-05 00:16:03 +0100

    Add components to App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Header } from './components/Header';
+import { SideNav } from './components/SideNav';
+import { Footer } from './components/Footer';
+import { Main } from './components/Main';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-screen h-screen relative">
+      <Header />
+      <div className="flex w-screen h-l">
+        <SideNav />
+        <Main />
+      </div>
+      <Footer />
     </div>
   );
 }
