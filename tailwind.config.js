473a7ee6d721d0a59beb894f16fd8afc35bfcf11
Timur Buchberger <timur,buch@users.noreplay.github.com>
2022-02-04 23:18:23 +0100

    Add custom colors  and heights to config

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'brand-header': '#f9fafc',
+        'brand-main': '#f5f6fa',
+        footer: '#eff1fa',
+        'highlight-bg': '#fef8f3',
+        'highlight-text': '#eb8b3d',
+        'highlight-button': '#e9812c',
+      },
+      height: {
+        sm: '10vh',
+        l: '80vh',
+      },
+    },
+  },
+  plugins: [],
+};
